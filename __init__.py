c671af143a92d06e64d199a1ee164e7a5917d7eb
CNLiuShuhan <143274270+CNLiuShuhan@users.noreply.github.com>
2023-10-15 20:44:18 +0800

    Update __init__.py

---
+++ __init__.py
@@ -0,0 +1,4 @@
+_help='Open the Control Panel and input "help" to get more help.'
+_copyright="Copyright (c) 2023~now Liu Shuhan.All Rights Reserved."
+_credits="Thanks to Python Corporation and the all of the supporters for this Model development.Ask Liu Shuhan for more information."
+_license="It was no license now,but it will may be license in the future."
