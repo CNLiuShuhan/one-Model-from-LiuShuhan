fa5d4228ee98d2a27926aa12963e4f3c79b30899
CNLiuShuhan <143274270+CNLiuShuhan@users.noreply.github.com>
2023-11-14 21:02:46 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
-_help='Open the Control Panel and input "help" to get more help.'
+_help='Please open the Control Panel and input "help" to get more help.'
 _copyright="Copyright (c) 2023~now Liu Shuhan.All Rights Reserved."
-_credits="Thanks to Python Corporation and the all of the supporters for this Model development.Ask Liu Shuhan for more information."
-_license="It was no license now,but it will may be license in the future."
+_credits="Thanks to Python Corporation and the all of the supporters for this Model development.\
+Ask Liu Shuhan for more information."
+_license="It is free for all of the peoples,please note that this model has not been released\
+ and get it by Liu Shuhan is the only safe channal."
